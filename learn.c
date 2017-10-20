c1f6b5fe1602f77d1375475e41c84de8850accf2
kendeg1 <kendeg1@gmail.com>
2017-10-20 17:12:24 -0400

    Update learn.c

--- learn.c
+++ learn.c
@@ -19,7 +19,7 @@ multiplication*/
     for(i = 0; i<cc; i++){
         mtx[i]=malloc(rc*sizeof(double));
         for(j = 0; j<rc;j++){
-        mtx[i][j]=matrix[i][j];
+        mtx[i][j]=matrix[j][i];
         }
     }
     for(i=0; i<cc;i++){
@@ -49,41 +49,88 @@ int main(int argc, char** argv){
     }
     fscanf(filepointer,"%d\n",&attribute);
 	printf("%d number of attributes\n",attribute);
+
 	columns = attribute + 1;
-	
+
 	printf("%d number of columns\n",columns);
 	fscanf(filepointer,"%d\n",&rows);
 	printf("%d number of rows\n",rows);
-        printf("\ntranspose test\n");
-        
-     double **m1 = malloc(rows*sizeof(double *));
+   
+   printf("\nThis is the original Train Matrix\n");
+   double **m1 = malloc(rows*sizeof(double *));
     for(i = 0; i<rows;i++){
         m1[i]=malloc(columns*sizeof(double));
         for(j=0;j<columns;j++){
           fscanf(filepointer,"%lf, \n",&m1[i][j]); 
           }      
     }
-    for(i = 0; i<rows;i++){
+   for(i = 0; i<rows;i++){
         for(j=0; j<columns; j++){
         printf("%lf\t",m1[i][j]);
         }
         printf("\t\n");
     }   
-       //THIS IS GIVING ME SEGV DURING RUNTIME
-     /*double **m1t = malloc(columns*sizeof(double *));
-      for(i = 0; i<columns;i++){
+    
+    printf("rows = %d\n",columns);
+	printf("columns = %d\n",rows);
+    printf("This is TRANSPOSE of ORIGINAL TRAIN MATRIX\n");
+   double **m1t = malloc(columns*sizeof(double *));
+    for(i = 0; i<columns;i++){
         m1t[i]=malloc(rows*sizeof(double));
         for(j=0;j<rows;j++){
-        fscanf(filepointer,"%lf, \n",&m1t[j][i]); 
+        m1t[i][j] = m1[j][i];
                }
     }
+    
     for(i = 0; i<columns;i++){
         for(j=0; j<rows; j++){
-        printf("%lf\t",m1t[j][i]);
+        printf("%lf\t",m1t[i][j]);
         }
         printf("\t\n");
-    }*/
+    }   
     
+    
+    printf("\nThis is Train Matrix with first column of 1s\n");
+   
+   double **mX = malloc(rows*sizeof(double *));
+    for(i=0; i <rows;i++){
+        mX[i]=malloc(columns*sizeof(double));
+        for(j=0;j<columns;j++){
+        if(j == 0){
+            mX[i][0] = 1;
+        }else{
+         mX[i][j] = m1[i][j-1];
+        }
+        }
+    }
+    for(i = 0; i<rows;i++){
+        for(j=0; j<columns; j++){
+        printf("%lf\t",mX[i][j]);
+        }
+        printf("\t\n");
+    }   
+    
+ 
+    printf("\nThis is TRANSPOSE of COLUMNS OF 1 TRAIN MATRIX\n");
+    double **mXt = malloc(columns*sizeof(double *));
+    for(i=0; i <columns;i++){
+        mXt[i]=malloc(rows*sizeof(double));
+        for(j=0;j<rows;j++){
+         mXt[i][j] = mX[j][i];
+        }
+        }
+  
+    
+    for(i = 0; i<columns;i++){
+        for(j=0; j<rows; j++){
+        printf("%lf\t",mXt[i][j]);
+        }
+        printf("\t\n");
+    }   
+    
+    printf("\nThis is original TEST MATRIX\n");
+     
+     
     //this is the test matrix
     filepointer = fopen(argv[2], "r");
     if (filepointer == NULL){
@@ -107,14 +154,13 @@ int main(int argc, char** argv){
         }
         printf("\t\n");
     }
-    //transposeMatrix(m2,rows,attribute);
    
    printf("\n");
    double **m2t = malloc(attribute*sizeof(double *));
     for(i = 0; i<attribute;i++){
         m2t[i]=malloc(rows*sizeof(double));
         for(j=0;j<rows;j++){
-        fscanf(filepointer,"%lf, \n",&m2t[j][i]); 
+        m2t[i][j] = m2[j][i]; 
                }
     }
     
